4293bb44d1a6e85e123a82d7918045b3904b2d65
JR <jose.alonso.programmer@gmail.com>
2022-11-16 17:07:07 +0100

    Using Same, NotSame, true and false assertions.

--- 1.00-starting-project/src/test/java/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/DemoUtilsTest.java
@@ -14,22 +14,42 @@ class DemoUtilsTest {
     }
 
     @Test
-    // @DisplayName("Equals and Not Equals")
-    void test_Equals_And_Not_Equals() {
+    @DisplayName("Equals and Not Equals")
+    void testEqualAndNotEquals() {
 
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
     }
 
     @Test
-    // @DisplayName("Null and Not Null")
-    void test_Null_And_Not_Null() {
+    @DisplayName("Null and Not Null")
+    void testNullAndNotNull() {
 
         String nullStr = null;
         String myStr = "a string";
 
         assertNull(demoUtils.checkNull(nullStr), "Object should be null");
         assertNotNull(demoUtils.checkNull(myStr), "Object should not be null");
+    }
+
+    @DisplayName("Same and Not Same")
+    @Test
+    void testSameAndNotSame() {
+
+        String myStr = "a string";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to same object");
+        assertNotSame(myStr, demoUtils.getAcademy(), "Objects should not refer to same object");
+    }
+
+    @DisplayName("True and False")
+    @Test
+    void testTrueFalse() {
+        int gradeOne = 10;
+        int gradeTwo = 5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo),  "This should return true");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
     }
 
     /*
