a4d9ee68a0123cfcacc77412e65285c16483e49e
JR <jose.alonso.programmer@gmail.com>
2022-11-17 13:52:25 +0100

    Running the test using different package names.

---
+++ 2.00-starting-project/src/test/java/com/josealonso/test/ApplicationExampleTest.java
@@ -0,0 +1,14 @@
+package com.josealonso.test;
+
+import com.josealonso.component.MvcTestingExampleApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class ApplicationExampleTest {
+
+    @Test
+    void basicTest() {
+
+    }
+}
