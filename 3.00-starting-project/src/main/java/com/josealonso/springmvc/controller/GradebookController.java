f3ec307c8e70d1141e1e58c57969875bc23d6ac6
JR <jose.alonso.programmer@gmail.com>
2022-11-20 01:45:38 +0100

    Testing the main controller endpoint.

--- 3.00-starting-project/src/main/java/com/josealonso/springmvc/controller/GradebookController.java
+++ 3.00-starting-project/src/main/java/com/josealonso/springmvc/controller/GradebookController.java
@@ -1,28 +1,36 @@
 package com.josealonso.springmvc.controller;
 
 import com.josealonso.springmvc.models.Gradebook;
-import com.josealonso.springmvc.models.*;
+import com.josealonso.springmvc.service.StudentAndGradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GradebookController {
 
-	@Autowired
-	private Gradebook gradebook;
+    @Autowired
+    private StudentAndGradeService studentService;
 
+    @Autowired
+    private Gradebook gradebook;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String getStudents(Model m) {
-		return "index";
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getStudents(Model m) {
+        //Iterable<CollegeStudent>
+        var collegeStudents = studentService.getGradebook();
+        m.addAttribute("students", collegeStudents);
+        return "index";
+        // return (String) m.getAttribute("students");
+    }
 
-
-	@GetMapping("/studentInformation/{id}")
-		public String studentInformation(@PathVariable int id, Model m) {
-		return "studentInformation";
-		}
+    @GetMapping("/studentInformation/{id}")
+    public String studentInformation(@PathVariable int id, Model m) {
+        return "studentInformation";
+    }
 
 }
--- 3.00-starting-project/src/test/com/josealonso/springmvc/GradebookControllerTest.java
+++ 3.00-starting-project/src/test/com/josealonso/springmvc/GradebookControllerTest.java
@@ -1,5 +1,7 @@
 package com.josealonso.springmvc;
 
+import com.josealonso.springmvc.models.CollegeStudent;
+import com.josealonso.springmvc.models.GradebookCollegeStudent;
 import com.josealonso.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +12,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestPropertySource("/application.properties")
 @AutoConfigureMockMvc
@@ -30,6 +42,28 @@ public class GradebookControllerTest {
     public void setupDatabase() {
         jdbc.update("INSERT INTO student VALUES (?, ?, ?, ?)",
                 1, "John", "Smith", "john@school.com");
+    }
+
+    @Test
+    public void getStudentsHttpRequest() throws Exception {
+        CollegeStudent studentOne = new GradebookCollegeStudent("Eric", "Rostand",
+                "eric@school.com");
+
+        CollegeStudent studentTwo = new GradebookCollegeStudent("Alice", "Rostand",
+                "alice@school.com");
+
+        List<CollegeStudent> collegeStudentList = List.of(studentOne, studentTwo);
+
+        when(studentAndGradeServiceMock.getGradebook()).thenReturn(collegeStudentList);
+
+        assertIterableEquals(collegeStudentList, studentAndGradeServiceMock.getGradebook());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "index");
     }
 
     @AfterEach
